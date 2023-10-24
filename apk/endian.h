c8dd12f81e6f83d5b11f8e71c7369281b1dcab4e
Robin Southern <betajaen@users.noreply.github.com>
2023-10-24 15:58:34 +0100

    constexpr-less endian code.

--- apk/endian.h
+++ apk/endian.h
@@ -23,37 +23,41 @@
 
 namespace apk::endian {
 
-    constexpr size_t Little = 0;
-    constexpr size_t Big = 1;
+    constexpr int Little = 0;
+    constexpr int Big = 1;
     
 #if defined(__AMIGA__)
-    constexpr size_t Native = Big;
+    constexpr int Native = Big;
 #else
-    constexpr size_t Native = Little;
+    constexpr int Native = Little;
 #endif
 
     template<typename T>
     struct unaligned_pod {
         T val;
     } __attribute__((__packed__, __may_alias__));
-    
-    template<typename T, size_t EFrom, size_t ETo>
-    inline T endian_swap(const T& value) {
-        if constexpr (EFrom == ETo) {
-            return value;
-        }
-        else {
-            if constexpr (sizeof(T) == 4) {
-                return (T) __builtin_bswap32(value);
-            }
-            else if constexpr (sizeof(T) == 2) {
-                return (T) __builtin_bswap16(value);
-            }
-            else {
-                static_assert((sizeof(T) == 2 || sizeof(T) == 4), "Unsupported type!");
-            }
-        }
-    }
+
+    template<typename T, int EFrom, int ETo>
+    T endian_swap(const T& value);
+
+    template<>  inline uint16 endian_swap<uint16, Little, Little>(const uint16& v) { return v; }
+    template<>  inline uint16 endian_swap<uint16, Big, Big>(const uint16& v)       { return v; }
+    template<>  inline uint16 endian_swap<uint16, Little, Big>(const uint16& v)    { return __builtin_bswap16(v); }
+    template<>  inline uint16 endian_swap<uint16, Big, Little>(const uint16& v)    { return __builtin_bswap16(v); }
+    template<>  inline int16  endian_swap<int16, Little, Little>(const int16& v) { return v; }
+    template<>  inline int16  endian_swap<int16, Big, Big>(const int16& v)       { return v; }
+    template<>  inline int16  endian_swap<int16, Little, Big>(const int16& v)    { return __builtin_bswap16(v); }
+    template<>  inline int16  endian_swap<int16, Big, Little>(const int16& v)    { return __builtin_bswap16(v); }
+
+    template<>  inline uint32 endian_swap<uint32, Little, Little>(const uint32& v) { return v; }
+    template<>  inline uint32 endian_swap<uint32, Big, Big>(const uint32& v)       { return v; }
+    template<>  inline uint32 endian_swap<uint32, Little, Big>(const uint32& v)    { return __builtin_bswap32(v); }
+    template<>  inline uint32 endian_swap<uint32, Big, Little>(const uint32& v)    { return __builtin_bswap32(v); }
+    template<>  inline int32  endian_swap<int32, Little, Little>(const int32& v)  { return v; }
+    template<>  inline int32  endian_swap<int32, Big, Big>(const int32& v)       { return v; }
+    template<>  inline int32  endian_swap<int32, Little, Big>(const int32& v)    { return __builtin_bswap32(v); }
+    template<>  inline int32  endian_swap<int32, Big, Little>(const int32& v)    { return __builtin_bswap32(v); }
+
 
 
     template<typename T, size_t ESourceEndian, size_t EDestEndian = Native>
